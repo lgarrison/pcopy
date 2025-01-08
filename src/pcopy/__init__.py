5682d9514a47d343915ea5ca47953d93af722bc3
Lehman Garrison <lgarrison@flatironinstitute.org>
2025-01-08 12:34:51 -0500

    Handle file copies as well as directories

--- src/pcopy/__init__.py
+++ src/pcopy/__init__.py
@@ -1,9 +1,13 @@
 import concurrent.futures
 import shutil
 from pathlib import Path
+from typing import TypeAlias
+from os import PathLike
 
 import typer
 from typing_extensions import Annotated
+
+StrPath: TypeAlias = str | PathLike[str]
 
 
 class MultithreadedCopier(concurrent.futures.ThreadPoolExecutor):
@@ -25,36 +29,61 @@ class MultithreadedCopier(concurrent.futures.ThreadPoolExecutor):
 
 
 def pcopy(
-    src_dir: list[Path],
-    dest_dir: Path,
+    sources: Annotated[
+        list[Path],
+        typer.Argument(metavar='SRC...', help='Source files or directories'),
+    ],
+    dest: Annotated[Path, typer.Argument(help='Destination file or directory')],
     workers: Annotated[
-        int, typer.Option("--workers", "-w", help="Number of parallel workers")
+        int, typer.Option('--workers', '-w', help='Number of parallel workers')
     ] = 16,
     sendfile: Annotated[
         bool,
         typer.Option(
-            "--sendfile", "-s", help="Whether to use the sendfile syscall for copies"
+            '--sendfile', '-s', help='Whether to use the sendfile syscall for copies'
         ),
     ] = False,
 ):
-    if isinstance(src_dir, (str, Path)):
-        src_dir = [src_dir]
-    src_dir = [Path(src) for src in src_dir]
+    if isinstance(sources, (str, PathLike)):
+        sources = [sources]
+    sources = [Path(src) for src in sources]
 
     # Warning: if using pcopy as a library, note the global state
     shutil._USE_CP_SENDFILE = sendfile
 
-    dest_dir_exists = dest_dir.is_dir()
+    dest_dir_exists = dest.is_dir()
+
+    # With multiple sources, dest must be a directory
+    if len(sources) > 1 and not dest_dir_exists:
+        raise typer.BadParameter(
+            'Destination must be a directory when copying multiple sources'
+        )
+
     with MultithreadedCopier(max_workers=workers) as copier:
-        for src in src_dir:
+        for src in sources:
             # Behave like `cp -r src dest` if dest exists
-            dst = dest_dir / src.name if dest_dir_exists else dest_dir
-            shutil.copytree(src, dst, copy_function=copier.copy, dirs_exist_ok=True)
+            dst = dest / src.name if dest_dir_exists else dest
+
+            if src.samefile(dst):
+                raise typer.BadParameter(
+                    'src and dest are the same: '
+                    f'{src} -> {dst}'
+                )
+
+            if src.is_file():
+                copier.copy(src, dst)
+            else:
+                if src in dst.parents:
+                    raise typer.BadParameter(
+                        'Cannot copy a directory into itself: '
+                        f'{src} -> {dst}'
+                    )
+                shutil.copytree(src, dst, copy_function=copier.copy, dirs_exist_ok=True)
 
 
 def main():
     typer.run(pcopy)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
